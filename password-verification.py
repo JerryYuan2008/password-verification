7f2b507cc6161696c6f1bbe4c5c4925d67389a7e
Jerry Yuan <yuanminxun2008@gmail.com>
2018-10-18 02:54:17 +0800

    second version

--- password-verification.py
+++ password-verification.py
@@ -2,10 +2,13 @@
 x = 3
 password = 'a123456'
 while x > 0:
+    x = x-1
     pwd = input('please give your password:')
     if pwd == password:
         print('access successfully')
         break
     else:
-        x = x-1
-        print('wrong password!you still have ', x,'times remaining opportunities to retry')
+        if x > 0:
+            print('worng password!you still have', x,'times remaining opportunities to retry')
+        else:
+            print('you have already ran out of your opportunities')
